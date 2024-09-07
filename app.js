1545ff9992f99b14309ad0faf04a78a1cf4a6ee1
Jadson Reis <jrdesign9@gmail.com>
2024-09-07 20:08:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Função para abrir o popup
+    // Função para abrir o popup de aviso
     function openPopup() {
         document.getElementById('aviso_groot').style.display = 'flex';
     }
 
-    // Função para fechar o popup
+    // Função para fechar o popup de aviso
     function closePopup() {
         document.getElementById('aviso_groot').style.display = 'none';
     }
 
-    // Exibir o popup após 5 segundos
+    // Exibe o popup de aviso após 5 segundos
     setTimeout(openPopup, 20);
 
-    // Adicionar evento de clique para fechar o popup
-    document.getElementById('close-popup').addEventListener('click', closePopup);
+    // Adiciona evento de clique para fechar o popup de aviso
+    document.getElementById('fechar-popup').addEventListener('click', closePopup);
 
-    // Implementar busca ao pressionar Enter
+    // Implementa a busca ao pressionar Enter ou clicar no botão
     const inputField = document.querySelector('.pesquisar-bar input');
     const searchButton = document.getElementById('search-button');
     const searchMessage = document.querySelector('.pesquisar-message');
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     searchButton.addEventListener('click', search);
 
+    // Função para realizar a busca
     function search() {
         const query = inputField.value;
         if (query) {
@@ -37,23 +38,42 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Abrir o popup do artigo
-    const readMoreButtons = document.querySelectorAll('.read-more');
-    const articlePopup = document.getElementById('article-popup');
-    const articleContent = document.getElementById('article-content');
-    const closeArticlePopup = document.getElementById('close-article-popup');
+    // Abre o popup do artigo
+    const readMoreButtons = document.querySelectorAll('.leia-mais');
+    const articlePopup = document.getElementById('artigo-popup');
+    const articleContent = document.getElementById('artigo-content');
+    const closeArticlePopup = document.getElementById('fechar-artigo-popup');
 
     readMoreButtons.forEach(button => {
         button.addEventListener('click', () => {
             const content = button.dataset.content;
             const title = button.dataset.title;
 
-            articleContent.innerHTML = content;
-            articleContent.querySelector('h3').textContent = title;
+            // Cria um novo elemento div para o conteúdo do popup
+            const popupContent = document.createElement('div');
+            popupContent.classList.add('popup-artigo-content');
+            
+            // Cria um novo elemento h3 para o título
+            const popupTitle = document.createElement('h3');
+            popupTitle.textContent = title;
+
+            // Remove o título do conteúdo do popup
+            let cleanedContent = content.replace(/<h3>(.*?)<\/h3>/g, ''); 
+
+            // Adiciona o conteúdo do popup ao novo elemento div
+            popupContent.appendChild(popupTitle);
+            // Use innerHTML para inserir o conteúdo HTML
+            popupContent.innerHTML += cleanedContent; 
+
+            // Adiciona o novo elemento div ao popup
+            articleContent.innerHTML = ''; // Limpa o conteúdo anterior
+            articleContent.appendChild(popupContent);
+
             articlePopup.style.display = 'flex';
         });
     });
 
+    // Fecha o popup do artigo
     closeArticlePopup.addEventListener('click', () => {
         articlePopup.style.display = 'none';
     });
